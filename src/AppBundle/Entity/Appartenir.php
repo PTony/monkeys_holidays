5c9d5f2bf8a87c827bfc9c9134eacaf2f70f72de
PTony <jokr.01@gmail.com>
2016-06-17 12:36:02 +0200

    modif de ouf

--- src/AppBundle/Entity/Appartenir.php
+++ src/AppBundle/Entity/Appartenir.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -34,6 +33,8 @@ class Appartenir
     * @ORM\JoinColumn(name="group_id", referencedColumnName="id")
     */
     private $group_id;
+
+
     /**
      * Get id
      *
@@ -43,4 +44,53 @@ class Appartenir
     {
         return $this->id;
     }
+
+
+    /**
+     * Get user_id
+     *
+     * @return integer 
+     */
+    public function getUserID()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set user_id
+     *
+     * @param string $user_id
+     * @return Appartenir
+     */
+    public function setUserID($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get group_id
+     *
+     * @return integer 
+     */
+    public function getGroupID()
+    {
+        return $this->group_id;
+    }
+
+    /**
+     * Set group_id
+     *
+     * @param string $group_id
+     * @return Appartenir
+     */
+    public function setGroupID($group_id)
+    {
+        $this->group_id = $group_id;
+
+        return $this;
+    }
+
+
 }
